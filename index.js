910c2d06533293144f8c0ca673c283cfc98192d6
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-07-02 11:43:01 +0300

    Done with assignments

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
+reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  
+
+  
+
+
+
 */
+//create an array from the input string
+//reverse the array
+//create a string from the reversed array
+//
+function reversed(string){
+  //return the reversed string
+  return string.split('').reverse().join('')
+}
+function isPalindrome(string){
+  // If one of the condition is true it will return true 
+  return  string === reversed(string) ? true : false
+}
+// calling the function 
+isPalindrome('tobor')
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
